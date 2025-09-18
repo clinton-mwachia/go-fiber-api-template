bd2aa156d933e2a006a84de8415bb59221ed9907
Clinton Moshe <clintonmwachia9@gmail.com>
2025-09-18 13:07:21 +0300

    UPDATE: User the Load Function in main file

--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/clinton-mwachia/go-fiber-api-template/config"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// load env file
-	if err := godotenv.Load(); err != nil {
-		log.Println("⚠️  No .env file found, using system env")
-	}
+	// load env
+	config.Load()
+	// connect DB
 	config.ConnectDB()
 	fmt.Println("Hello go fiver template")
 }
