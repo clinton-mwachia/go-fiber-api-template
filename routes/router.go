d0cfee6c7efa47196ac6cb3d84373785215348a1
Clinton Moshe <clintonmwachia9@gmail.com>
2025-09-18 13:33:46 +0300

    ADDED: user test route

---
+++ routes/router.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/clinton-mwachia/go-fiber-api-template/controllers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func SetUpRouter(app *fiber.App) {
+	app.Use(logger.New())
+
+	api := app.Group("/api")
+
+	// users routes
+	api.Get("/", controllers.TestUserControler)
+}
